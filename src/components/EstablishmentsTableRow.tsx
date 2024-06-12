a9b4c5dd9dd7e198d50839e218c4511bef246435
Martin Heteš <martin@hetes.eu>
2024-06-12 16:50:18 +0200

    user story 1: font size update

--- src/components/EstablishmentsTableRow.tsx
+++ src/components/EstablishmentsTableRow.tsx
@@ -1,8 +1,12 @@
+const rowStyle: { [key: string]: string | number } = {
+  fontSize: "20px",
+};
+
 export const EstablishmentsTableRow: React.FC<{
   establishment: { [key: string]: string } | null | undefined;
 }> = ({ establishment }) => {
   return (
-    <tr>
+    <tr style={rowStyle}>
       <td>{establishment?.BusinessName}</td>
       <td>{establishment?.RatingValue}</td>
     </tr>
